85244fa8c95cf5d2c119c0cd6a63be2db7080950
Felipe Eduardo <Felipeeduardol7@outlook.com>
2023-03-07 17:47:52 -0300

    :construction: feat:schedule page and update-profile page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  domains: ['https://lh3.googleusercontent.com/'],
 }
 
 module.exports = nextConfig
